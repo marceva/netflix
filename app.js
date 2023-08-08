02ce774dd3c65df244f3feed3eb37b073336ac0b
Marcelo Vargas <marcevargasaguilar01@gmail.com>
2023-08-07 20:06:46 -0600

    Creating initial files

---
+++ app.js
@@ -0,0 +1,6 @@
+//IIFE
+(function(){
+    "use strict";// evitar fenomenos raros, se comporta mas rigido y mas seguro
+    alert('JavaScript Working!'); //testing code
+    console.log(data);
+})(); // no se ejecuta hasta que termine todo lo demnas 
